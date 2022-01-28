9dec627c37faff8d256b36faba64a982ac8f0278
Mario Günzel <mario@Air-von-Mario.fritz.box>
2022-01-28 12:40:16 +0100

    testing tda function

--- res/our_analysis.py
+++ res/our_analysis.py
@@ -40,14 +40,22 @@ def tda_with_carry_in(hp_period_wcet, wcet_this, stop_cond=None, start_val=0):
     """Compute WCRT using TDA with carry in.
     hp_period_wcet = list of (period, wcet)-tuples from higher priority tasks
     wcet_this = WCET of the task under analysis
+
+    Notes:
+        - blockfct = math.ceil is the standard TDA function
+        - blockfct = math.floor +1 is what we utilize here
     """
 
+    # blockfct = math.ceil  # standard way
+    blockfct = lambda x: math.floor(x) + 1
+
     def blocked(interval_length, period, wcet):
-        return math.ceil((interval_length + period + wcet) / period) * wcet
+        return blockfct((interval_length + period - wcet) / period) * wcet
 
     t = start_val
     while True:
         val = wcet_this + sum(blocked(t, period, wcet) for period, wcet in hp_period_wcet)
+        print(t, val, sum(blocked(t, period, wcet) for period, wcet in hp_period_wcet))  # debug
         if val <= t:
             assert val == t  # need exact TDA result for computation of Rplus and Rminus
             # if val != t:
@@ -57,3 +65,16 @@ def tda_with_carry_in(hp_period_wcet, wcet_this, stop_cond=None, start_val=0):
             raise ValueError('A response time under this setting could not be derived', hp_period_wcet, wcet_this,
                              stop_cond, start_val)
         t = val
+
+
+if __name__ == '__main__':
+    """Some testing"""
+
+    # Test 1:
+    print('===TEST 1===')
+    print(tda_with_carry_in([], 5), 'should be 5')
+    print(tda_with_carry_in([[10, 1]], 5), 'should be 7')
+    print(tda_with_carry_in([[2, 1]], 5), 'should be 12')
+    print(tda_with_carry_in([[10, 1], [5, 2]], 5), 'should be 16')
+    print(tda_with_carry_in([[10, 1], [5, 2]], 6), 'should be 19')
+    print(tda_with_carry_in([[10, 1], [5, 2]], 8), 'should be 24')
