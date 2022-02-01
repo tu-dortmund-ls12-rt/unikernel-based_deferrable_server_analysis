18ebca0f42fb8d2d0211d092a01e217b595a23d5
Mario Günzel <mario@MacBook-Air-von-Mario.fritz.box>
2022-02-01 19:18:57 +0100

    plot make whiskers larger

--- res/plot.py
+++ res/plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def plot(data, filename, xticks=None, title=''):
     fig, ax = plt.subplots()
     ax.set_title(title)
-    ax.boxplot(data)
+    ax.boxplot(data, whis=1000)
     if xticks is not None:
         plt.xticks(list(range(1, len(xticks) + 1)), xticks)
     # plt.show()
