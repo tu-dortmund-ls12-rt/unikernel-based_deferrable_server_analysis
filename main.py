da16b117f60ee133095e70d1a88d6ca8846e2ae4
Mario Günzel <mario@Air-von-Mario.fritz.box>
2022-02-01 08:32:27 +0100

    experiment to main file

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 import sys
 import os
 import getopt
+from res import benchmark, our_analysis, rtc_cb
+import pickle
+from multiprocessing import Pool
+
+
+# from res import benchmark
 
 
 def print_usage():
@@ -12,9 +18,27 @@ def print_usage():
 def check_or_make_directory(dirname):
     if not os.path.exists(dirname):
         os.makedirs(dirname)
+        print(f'Directory {dirname} created')
+
+
+def write_data(filename, data):
+    file = open(filename, 'wb')
+    pickle.dump(data, file)
+    file.close()
+    print(f'Data written to {filename}')
+
+
+def load_data(filename):
+    file = open(filename, 'rb')
+    data = pickle.load(file)
+    file.close()
+    print(f'Data loaded from {filename}')
+    return data
 
 
 if __name__ == "__main__":
+
+    num_processors = 1
 
     # =====args=====
     try:
@@ -28,3 +52,24 @@ if __name__ == "__main__":
             sys.exit()
         elif opt in ("-s",):  # define which part of the code is being executed
             code_switch = arg
+
+    if code_switch == '1':
+        num_systems = 100
+
+        # Create systems:
+        check_or_make_directory('data/1setup')
+        systems = benchmark.make_system(10, 0.1, 0.4, listof=num_systems)
+        write_data(f'data/1setup/systems{code_switch}.pkl', systems)
+
+        # Makes experiments
+        with Pool(num_processors) as p:
+            res_our = p.map(our_analysis.wcrt_analysis, systems)
+
+        with Pool(num_processors) as p:
+            res_rtc = p.map(rtc_cb.wcrt_analysis, systems)
+
+        check_or_make_directory('data/2results')
+        write_data(f'data/2results/res_our{code_switch}.pkl', res_our)
+        write_data(f'data/2results/res_rtc{code_switch}.pkl', res_rtc)
+
+        # Plot data
